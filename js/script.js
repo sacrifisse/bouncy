4b652c2b2f7fbb8c99d2b7a7cbf38310424c8416
sacrifisse <79392020+sacrifisse@users.noreply.github.com>
2022-07-13 14:24:03 +0300

    mobile menu fixes

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.menu-burger__header').click(function(){
         $('.menu-burger__header').toggleClass('open-menu');
         $('.header__nav').toggleClass('open-menu');
+        $('header').toggleClass('bg');
         $('body').toggleClass('fixed-page');
     });
 });
